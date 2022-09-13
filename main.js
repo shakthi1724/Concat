12b52d9fd38927ee134d8fba4c0a3f8d2fd712f2
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:08:07 +0530

    repush

---
+++ main.js
@@ -0,0 +1,17 @@
+/*var name1=["Sha"];
+console.log("Original array1:",name1);
+var name2=["kthi"]
+console.log("Original array2:",name2);
+console.log("Length of original array1:",name1.length);
+console.log("Length of original array2:",name2.length);
+var name3=name1.concat(name2);
+console.log("Method:concat();");
+console.log("Change in array:"+name3);
+console.log("Length of array after reverse(); is ",name3.length);
+*/
+
+
+var a ="sha";
+var b="kthi";
+var c=a.concat(b);
+console.log(c);
